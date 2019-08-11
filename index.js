d35ab2dd6b6777ab9399dfa6e8bc34080a3ecda8
kandrobat <dmytro18wa@gmail.com>
2019-08-10 22:09:43 -0700

    endpoints defined

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
 const express = require('express');
     morgan = require('morgan');
+    bodyParser = require('body-parser');
+    uuid = require('uuid');
 
 const app = express();
 
+app.use(bodyParser.json());
+
 let myMovies = [{
     title: 'Fight Club',
-    directed: 'David Fincher'
+    description: ['Fight Club is a 1999 film based on the 1996 novel',
+                    'of the same name by Chuck Palahniuk'],  
+                    /* broke it for readability of code */
+    genre: 'Drama',
+    directed: 'David Fincher',
+    imageURL: '#'
     },
     {
         title: 'Fear and Loathing in Las Vegas',
@@ -33,7 +42,8 @@ let myMovies = [{
     },
     {
         title: 'Four Rooms',
-        directed: 'Allison Anders, Alexandre Rockwell, Robert Rodriguez, Quentin Tarantino'
+        directed: ['Allison Anders', 'Alexandre Rockwell', 
+                    'Robert Rodriguez', 'Quentin Tarantino']
     },
     {
         title: 'The Science of Sleep',
@@ -45,6 +55,14 @@ let myMovies = [{
     }
 ]
 
+let users = [{
+    username: 'JohnDoe',
+    email: 'johndoe@email.com',
+    password: 'swordfish',
+    dob: '01/01/2001',
+    favorites: []
+}]
+
 //morgan middleware library is used to log all requests
 app.use(morgan('common'));
 
@@ -53,12 +71,94 @@ app.get('/movies', function(req, res) {
     res.json(myMovies)
 });
 
+// get data about a single movie by title
+app.get('/movies/:title', function (req, res) {
+    res.json(myMovies.find(function (movie) {
+        return movie.title === req.params.title
+    }));
+});
+
+// get data about a genre by title
+app.get('/movies/:title/:genre', function (req, res) {
+    let movie = myMovies.find((movie) => {
+        return movie.title === req.params.title
+    });
+
+    if (movie) {
+        res.status(201).send(movie.title + ' has ' + movie.genre + ' genre');
+    } else {
+        res.status(404).send('Movie with ' + req.params.title + ' title was not found');
+    }
+});
+
+// get data about a director by name
+app.get('/directed/:name', function (req, res) {
+    res.send('Requested page is under construction! Please, come back shortly!');
+});
+
+// allow new users to register
+app.post('/users', function (req, res) {
+    let newUser = req.body;
+
+    if (!newUser.username) {
+        const message = 'Please, enter a username';
+        res.status(400).send(message);
+    } else {
+        newUser.id = uuid.v4();   /* gives a unique id */
+        users.push(newUser);
+        res.status(201).send(newUser);
+    }
+});
+
+// allow users to update their info (example for username by id)
+app.put('/users/:id/:username', function (req, res) {
+    let user = users.find((user) => {
+        return user.id === req.params.id
+    });
+
+    if (user) {
+        res.status(201).send('You have changed your username to ' 
+        + req.params.username)
+    } else {
+        res.status(404).send('We are not able to find your id in the system')
+    }
+});
+
+// allow users to add a movie to their list of favorites
+app.post('/users/:username/:favorites', function (req, res) {
+    let newFav = req.body;
+
+    if (!newFav.favorites) {
+        const message = 'Title is missing';
+        res.status(400).send(message);
+    } else {
+        users.favorites.push(newFav);
+        res.status(201).send(newFav);
+    }
+});
+
+// allow users to remove a movie from their list of favorites
+app.delete('/users/:username/:favorites', function (req, res) {
+    res.send('Your favorite movie has been deleted');
+});
+
+// allow existing users to deregister using id
+app.delete("/users/:id", (req, res) => {
+    let user = users.find((user) => { 
+        return user.id === req.params.id });
+  
+    if (user) {
+      users.filter(function(obj) { return obj.id !== req.params.id });
+      res.status(201).send('You have been successfully deregistered')
+    }
+});
+
 // returns a default textual response
 app.get('/', function(req, res) {
     res.send('What is your favorite movie?')
 });
 
-//use express.static to serve “documentation.html” file from the public folder
+//use express.static to serve "documentation.html" file from the public folder
 app.use(express.static('public'));
 
 // error-handling middleware function to log all application-level errors to the terminal
